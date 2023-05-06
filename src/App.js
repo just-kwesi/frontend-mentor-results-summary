740c622005107a0dc9d1fdd01f784f5d124be82e
Frederick Tetteh <kwesiossom@gmail.com>
2023-05-06 19:08:47 -0400

    landmark added to contain page

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Summary, Results } from './components';
 function App() {
   return (
     <div className="app">
-      <div className="results-container">
+      <div className="results-container" role="main">
         <Results />
         <Summary />
       </div>
